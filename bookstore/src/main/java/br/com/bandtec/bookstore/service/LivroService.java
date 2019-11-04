0532f3bbd076659cc3d779741d0c60215cd773ad
Matheus Gomes <hotmus8500@gmail.com>
2019-11-04 20:45:25 -0300

    initial commit

---
+++ bookstore/src/main/java/br/com/bandtec/bookstore/service/LivroService.java
@@ -0,0 +1,33 @@
+package br.com.bandtec.bookstore.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.com.bandtec.bookstore.domain.Livro;
+
+@Service
+public class LivroService {
+	private List<Livro> livros;
+
+	public List<Livro> buscarPorTema(String tema) {
+		List<Livro> resultados = new ArrayList<Livro>();
+		for (Livro livro : todosLivros()) {
+			if(livro.comTema(tema)) {
+				resultados.add(livro);
+			}
+		}
+		return resultados;
+	}
+	
+	private List<Livro> todosLivros(){
+		List<Livro> livros = new ArrayList<Livro>();
+		
+		livros.add(new Livro("50 tons de azul", "Comedia"));
+		livros.add(new Livro("A morte do JavaScripto", "Historia"));
+		livros.add(new Livro("Kotlin ou Java", "TI"));
+		livros.add(new Livro("PALAMEIRAS UM CAMPEÃO INCOMPREENDIDO", "Historia"));
+		return livros;
+	}
+}
